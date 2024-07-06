5448f8196effdfae86c1ecb234fd31c15d58b126
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-07-06 18:31:51 +0700

    SW change 5/7

--- Internship/Models/InternshipDbContext.cs
+++ Internship/Models/InternshipDbContext.cs
@@ -1,4 +1,6 @@
-﻿namespace Internship.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Internship.Models
 {
     public class InternshipDbContext : DbContext
     {
@@ -7,23 +9,54 @@
         {
         }
 
-        public DbSet<users> Users { get; set; }
+        public DbSet<users> users { get; set; }
         public DbSet<Intern> Interns { get; set; }
-        public DbSet<programs> Programs { get; set; }
-        public DbSet<mentor> Mentors { get; set; }
+        public DbSet<programs> programs { get; set; }
+        public DbSet<mentor> mentor { get; set; }
         public DbSet<Intern_Programs> InternPrograms { get; set; }
         public DbSet<report> Reports { get; set; }
-        public DbSet<feedback> feedback { get; set; }
+        public DbSet<feedback> Feedback { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<users>().ToTable("users");
-            modelBuilder.Entity<Intern>().ToTable("Intern");
-            modelBuilder.Entity<programs>().ToTable("programs");
-            modelBuilder.Entity<mentor>().ToTable("mentor");
-            modelBuilder.Entity<Intern_Programs>().ToTable("Intern_Programs");
-            modelBuilder.Entity<report>().ToTable("report");
-            modelBuilder.Entity<feedback>().ToTable("feedback");
+            modelBuilder.Entity<users>();
+            modelBuilder.Entity<Intern>();
+            modelBuilder.Entity<programs>();
+            modelBuilder.Entity<mentor>();
+            modelBuilder.Entity<Intern_Programs>();
+            modelBuilder.Entity<report>();
+            modelBuilder.Entity<feedback>();
+
+            modelBuilder.Entity<Intern>()
+                .HasOne(i => i.users)
+                .WithMany()
+                .HasForeignKey(i => i.UserId);
+
+            modelBuilder.Entity<Intern_Programs>()
+                .HasOne(ip => ip.Intern)
+                .WithMany()
+                .HasForeignKey(ip => ip.InternId);
+
+            modelBuilder.Entity<Intern_Programs>()
+                .HasOne(ip => ip.programs)
+                .WithMany()
+                .HasForeignKey(ip => ip.ProgramId);
+
+            modelBuilder.Entity<Intern_Programs>()
+                .HasOne(ip => ip.mentor)
+                .WithMany()
+                .HasForeignKey(ip => ip.MentorId);
+
+            modelBuilder.Entity<report>()
+                .HasOne(r => r.Intern_Programs)
+                .WithMany()
+                .HasForeignKey(r => r.InternProgramId);
+
+            modelBuilder.Entity<feedback>()
+                .HasOne(f => f.report)
+                .WithMany()
+                .HasForeignKey(f => f.ReportId);
         }
+   */
     }
 }
